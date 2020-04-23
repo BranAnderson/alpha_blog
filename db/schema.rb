5103f520cbf92f1de587f1305d1cdce4ba1805bd
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-04-23 10:21:00 -0700

    Added secure password to users table for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_213414) do
+ActiveRecord::Schema.define(version: 2020_04_23_170345) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_213414) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
