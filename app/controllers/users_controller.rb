9194139530f0f7b27f8350d281e90287f7a0ec58
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-04-23 12:59:36 -0700

    crated user index page to view all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
   
   def new
     @user = User.new
@@ -13,6 +13,14 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def show
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
