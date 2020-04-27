33a07ea628740fc8f814fcbb75e5abfdb6569f8f
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-04-27 10:01:02 -0700

    limiting access and actions based on specific logged in users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     #Call method to whitelist params for the article
     @article = Article.new(article_params)
     #Hard coding in a user_id to the new article
-    @article.user = User.first
+    @article.user = current_user
     #save the new article to the table
     if @article.save
       #if doing the above action was successful - returns true - then do the next items
