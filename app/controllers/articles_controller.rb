26c64a22f06446f533de15b9d3d1e9262cc9c02f
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-04-27 10:14:55 -0700

    finished authorization limitations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   #before we do any actions, call the set_article method, but only for the show, edit, update, and destroy methods
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     
@@ -78,6 +80,13 @@ class ArticlesController < ApplicationController
   def article_params
     #whitelists the title and description parameters of an article
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
   end
 
 end
